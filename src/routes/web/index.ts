e68be65ab3ba4c9b6c86cf4a7697e9e96cf876ac
Sayan "Riju" Chakrabarti <s26c.sayan@gmail.com>
2021-06-17 17:46:33 +0530

    feat(web routes): routes for web converted to ts

---
+++ src/routes/web/index.ts
@@ -0,0 +1,30 @@
+import { DocumentType } from '@typegoose/typegoose';
+import { LeanDocument } from 'mongoose';
+
+import express from 'express';
+const router = express.Router();
+
+import { User, UserClass } from '../../models/user';
+
+/* GET home page. */
+router.get('/resetpassword/:token', async (req, res) => {
+  try {
+    const { token } = req.params;
+    const now = new Date();
+    const user: LeanDocument<DocumentType<UserClass>> | null =
+      await User.findOne({
+        isActive: true,
+        'forgotpassword.token': token,
+        'forgotpassword.expiresAt': { $gte: now },
+      })
+        .select('email')
+        .lean()
+        .exec();
+    if (user === null) throw new Error('INVALID OR EXPIRED LINK');
+    return res.render('resetpassword', { handle: user.email, token });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
